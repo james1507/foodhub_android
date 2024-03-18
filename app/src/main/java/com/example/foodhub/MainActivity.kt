10255126802b243e0d7ab8565308174aadd0f2ba
ThanhBV <thanhbv@newwave.com.vn>
2024-03-18 18:43:50 +0700

    first push

---
+++ app/src/main/java/com/example/foodhub/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.foodhub
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, SplashActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/foodhub/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodhub.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
